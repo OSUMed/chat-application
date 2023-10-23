e42e7c7a27a3f4ff115e7a084946159848539ab0
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-10-22 17:06:39 -0700

    add stream method to get messages from message service in correct format

--- src/main/java/com/osumed/chatapplication/services/ChannelService.java
+++ src/main/java/com/osumed/chatapplication/services/ChannelService.java
@@ -1,8 +1,39 @@
 package com.osumed.chatapplication.services;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.osumed.chatapplication.domain.Person;
 
 @Service
 public class ChannelService {
-   
- 
+
+	@Autowired
+	MessagesService messagesService;
+
+	@Autowired
+	PersonService personService;
+
+	private List<ArrayList<String>> getMessages(){
+		List<ArrayList<String>> messages = messagesService.getMessages();
+		List<ArrayList<String>> formattedMessages = messages.stream()
+			    .map(innerList -> {
+			    		Long userId = Long.parseLong(innerList.get(1));
+			    		Person person = personService.getPerson(userId);
+			    		String username = person.getName();
+		    		    innerList.set(1, username); // Corrected set method usage
+			    	    return innerList;
+			    		}
+			    )
+			    .collect(Collectors.toList());
+		return formattedMessages;
+	}
+
+	private void addMessage(String message, Person person, String channel) {
+		messagesService.addMessage(message, person.getPersonId(), channel);
+	}
 }
--- src/main/java/com/osumed/chatapplication/services/MessagesService.java
+++ src/main/java/com/osumed/chatapplication/services/MessagesService.java
@@ -2,18 +2,19 @@ package com.osumed.chatapplication.services;
 
 import org.springframework.stereotype.Service;
 import java.util.ArrayList;
+import java.util.List;
 import java.util.Arrays;
 
 @Service
 public class MessagesService {
-    ArrayList<ArrayList<String>> allMessages = new ArrayList<ArrayList<String>>();
+	List<ArrayList<String>> allMessages = new ArrayList<ArrayList<String>>();
     
-    private void AddMessage(String message, Long userId, String channel) {
+    public void addMessage(String message, Long userId, String channel) {
         ArrayList<String> newMessage = new ArrayList<>(Arrays.asList(message, channel, String.valueOf(userId)));
         allMessages.add(newMessage); // Add the new message to the list
     }
     
-    private ArrayList<ArrayList<String>> getMessages(){
+    public List<ArrayList<String>> getMessages(){
     	return allMessages;
     }
 }
