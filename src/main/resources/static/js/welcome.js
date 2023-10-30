669fc163a0edf54b373d8e6fe66b1c345bbb2499
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-10-29 19:36:08 -0700

    recreate welcome.js with endpoint fetching

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -1,15 +1,48 @@
-// Cached elements
+// 1. DOM Element Grabbing (Cache elements)
 const body = document.getElementById("body");
 
-var userName = prompt("Please enter your name:", "Default Name");
-let paragraph = document.createElement("p");
-const textNode = document.createTextNode(
-  `Hello, ${userName}! How are you today?`
-);
-paragraph.appendChild(textNode);
-body.appendChild(paragraph);
+// 2. Function Definitions
+const promptUserId = () => {
+  return prompt("Please enter your name:", "Default Name");
+};
 
-if (userName != null) {
-  sessionStorage.setItem("userName", userName);
-  console.log("Hello " + userName + "! How are you today?");
+function generateUUID() {
+  // Browser API:
+  return crypto.randomUUID();
 }
+
+const sendUserIdToServer = (userName) => {
+  const bodyData = {
+    name: userName,
+    personId: generateUUID(),
+  };
+  const options = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: bodyData,
+  };
+  fetch("the endppint", options)
+    .then((res) => res.json())
+    .then((data) => {
+      sessionStorage.setItem("userName", data.name);
+      console.log("Hello " + data.name + "! How are you today?");
+    });
+};
+
+const introduceUser = () => {
+  let paragraph = document.createElement("p");
+  const textNode = document.createTextNode(
+    `Hello, ${userName}! How are you today?`
+  );
+  paragraph.appendChild(textNode);
+  body.appendChild(paragraph);
+};
+
+// 3. On Page Load Logic
+document.addEventListener("DOMContentLoaded", (event) => {
+  let userName = promptUserId();
+  sendUserIdToServer(userName);
+  introduceUser();
+});
