8cd66b85c6c34d12e7a4798de9a4f2fd07a45e4d
OSUMed <62990411+OSUMed@users.noreply.github.com>
2023-11-19 23:31:01 -0800

    changed refresh user prompt bug

--- src/main/resources/static/js/welcome.js
+++ src/main/resources/static/js/welcome.js
@@ -4,44 +4,50 @@ const url = "/api/user";
 
 // 2. Helper Functions
 const promptUserName = () => {
-  return prompt("Please enter your name:", "Default Name");
+	return prompt("Please enter your name:", "Default Name");
 };
 
 const storeUserInfoSessions = (name, userId) => {
-  sessionStorage.setItem("userName", name);
-  sessionStorage.setItem("userId", userId);
+	sessionStorage.setItem("userName", name);
+	sessionStorage.setItem("userId", userId);
 };
 
 const attachUserDialogToDom = (userName) => {
-  let paragraph = document.createElement("p");
-  const textNode = document.createTextNode(
-    `Hello, ${userName}! How are you today?`
-  );
-  paragraph.appendChild(textNode);
-  body.appendChild(paragraph);
+	let paragraph = document.createElement("p");
+	const textNode = document.createTextNode(
+		`Hello, ${userName}! How are you today?`
+	);
+	paragraph.appendChild(textNode);
+	body.appendChild(paragraph);
 };
 
 const sendUserIdToServer = (userName) => {
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: userName,
-  };
-  fetch(url, options)
-    .then((res) => res.json())
-    .then((data) => {
-      let name = data.name;
-      let userId = data.userId;
-      storeUserInfoSessions(name, userId);
-    });
+	const options = {
+		method: "POST",
+		headers: {
+			"Content-Type": "application/json",
+		},
+		body: userName,
+	};
+	fetch(url, options)
+		.then((res) => res.json())
+		.then((data) => {
+			let name = data.name;
+			let userId = data.userId;
+			storeUserInfoSessions(name, userId);
+		});
 };
 
 // 3. On Page Load Logic
 document.addEventListener("DOMContentLoaded", (event) => {
-  // Prompt User's name, send to server & save to sessions:
-  let userName = promptUserName();
-  sendUserIdToServer(userName);
-  attachUserDialogToDom(userName);
+	// Prompt User's name, send to server & save to sessions:
+	let userName;
+	if (!sessionStorage.getItem('hasVisited')) {
+		userName = promptUserName();
+		sendUserIdToServer(userName);
+
+		sessionStorage.setItem('name', userName);
+		sessionStorage.setItem('hasVisited', 'true');
+	}
+	attachUserDialogToDom(sessionStorage.getItem('name'));
 });
